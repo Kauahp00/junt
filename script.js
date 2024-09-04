f6a0cafcdfc116e945b1856d949a4404aff13877
Kauahp00 <132684345+Kauahp00@users.noreply.github.com>
2024-09-04 09:51:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function showNextModal(modalId) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => modal.style.display = 'none');
+    
+    const nextModal = document.getElementById(modalId);
+    if (nextModal) {
+        nextModal.style.display = 'flex';
+    }
+}
+
+
+// Exibe a primeira modal ao clicar no botão "INICIAR TESTE"
+document.querySelector('.btnIniciar').addEventListener('click', function() {
+    showNextModal('modal1');
+});
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+
+    // Redireciona para a página de jogo após o fechamento do modal
+    if (modalId === 'modal3') {
+        window.location.href = 'quest1.html';
+    }
+}
+
+
+
